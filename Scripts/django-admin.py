24513a89311ba2dfa95ab5b61173c07400fbc97e
kareemhamdy500 <kareemhamdy500@gmail.com>
2019-05-24 00:01:39 +0200

    remove Env

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kareem\desktop\web_projects\django_apps\assignment\assignment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
